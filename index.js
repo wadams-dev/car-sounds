5cdddcd69ba28dfa8d0c6a8a3cf3877bb9c43d99
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2020-07-16 18:45:16 -0400

    Add updated files

--- index.js
+++ index.js
@@ -30,23 +30,7 @@ function playSound (manufacturer){
       var v12 = new Audio("sounds/ferrari.mp3");
       v12.play();
       break;
-    case 'd':
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-    case 'j':
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case 'k':
-      var tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case 'l':
-      var tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
     default:
-      console.log(character);
+      console.log(manufacturer);
   }
 }
